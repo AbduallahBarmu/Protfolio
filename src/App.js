238864ffb00c7c520a55c842fc1ed6c9e3c061a3
unknown <Abduallah.imu2@gmail.com>
2022-06-08 18:39:11 +0300

    build protfolio structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
